b21d354636bd97bfeb4e66ba467d6f4e8bbe3ff0
Abhishek Kumar <abhi.ckp1002@gmail.com>
2019-01-12 14:25:16 +0530

    new portfolio update

--- js/main.js
+++ js/main.js
@@ -23,6 +23,15 @@
       });       
 
   	})
+	$(document).bind("contextmenu",function(e) {
+		e.preventDefault();
+	});
+
+	$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }
+	});
 
 
   	/*---------------------------------------------------- */
